783784008a5b01c5b044afad6a97f97a2220056c
Kroquetitaa <Kroquetita@gmail.com>
2022-07-13 11:30:20 +0200

    forEach

---
+++ theory/js/clase-12.js
@@ -0,0 +1,12 @@
+
+//ForEach
+
+const colors = ['Red','Blue','Yellow'];
+
+colors.forEach( color => {
+    if( color === 'Blue'){
+        console.log('Este es el rojo');
+    } else {
+        console.log(`No es el azul es ${color}`);
+    }
+});
