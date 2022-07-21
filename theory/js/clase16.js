e7138ec6f8e4eb3b237e94258e0cf69115ec8fe2
Kroquetitaa <Kroquetita@gmail.com>
2022-07-21 12:04:33 +0200

    repaso clase16

--- theory/js/clase16.js
+++ theory/js/clase16.js
@@ -14,3 +14,55 @@ matriz.forEach( (elementoIterado, index, array) => {
 let newMatriz = matriz.map( elementoIterado => {
     if (elementoIterado === 'Alberto') return elementoIterado
 }); 
+
+// Funciones
+const funcion = () => {
+
+}
+
+// Puedes retornar valores - despues del return no se ejecuta nada
+const returnfunction = () => {
+    return 'hola a todos';
+    console.log('hola');
+}
+
+// puedes tener parametros -> elementos de entrada y pueden tener el nombre que queramos
+const paramFunction = param => {
+    return param + 5;
+}
+
+// Para ejecutar una funcion  debemos invocarla
+returnfunction();
+paramFunction();
+
+// las funciones con return se suelen guardar los valores en variables
+
+let greeting = returnfunction();
+
+let sumaDecinco = paramFunction(25);
+
+
+const entrenador = (orden) => {
+    console.log('entrenador', orden);
+    central( orden );
+}
+const portero = (orden) => {
+    console.log('entrenador', orden);
+    central( orden );
+}
+const central = (orden) => {
+    console.log('entrenador', orden);
+    medioIzquierda( orden );
+}
+const medioIzquierda = (orden) => {
+    console.log('entrenador', orden);
+    medioDerecha( orden );
+}
+const medioDerecha = (orden) => {
+    console.log('entrenador', orden);
+    delantero( orden )
+}
+const delantero = (orden) => {
+    console.log('entrenador', orden);
+}
+portero();
